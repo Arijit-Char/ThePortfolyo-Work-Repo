111ad15e6382e5c02a432316b6b8904ab9a806c6
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-04-21 01:59:17 +0530

    Navmenu

--- src/components/Menu/Menuitem.jsx
+++ src/components/Menu/Menuitem.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-scroll';
+
 import './menu.scss';
 const variants = {
     open: {
@@ -21,12 +22,14 @@ const variants = {
 
 const colors = ['#FF008C', '#D309E1', '#9C1AFF', '#7700FF', '#4400FF', '#2600FF', '#1400FF'];
 
-export const MenuItem = ({ i, Links, Title }) => {
+export const MenuItem = ({ i, Links, Title,icon }) => {
     const style = { border: `2px solid ${colors[i]}` };
     return (
         <Link activeClass="active" to={Links} spy={true} smooth={true} hashSpy={true} offset={100} duration={500} delay={50} isDynamic={true}>
             <motion.li variants={variants} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-                <div className="icon-placeholder" style={style} />
+                <div className="icon-placeholder" style={style} > 
+                {icon}
+                </div>
                 <div className="text-placeholder" style={style}>
                     {Title}
                 </div>
--- src/components/Menu/Navigation.jsx
+++ src/components/Menu/Navigation.jsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import { motion } from 'framer-motion';
 import { MenuItem } from './Menuitem';
+import { FaHome } from "react-icons/fa";
+import { GiSkills } from "react-icons/gi";
+import { GoProjectSymlink } from "react-icons/go";
+import { GrServices } from "react-icons/gr";
+import { MdViewTimeline } from "react-icons/md";
+import { MdRateReview } from "react-icons/md";
+import { RiContactsFill } from "react-icons/ri";
+
 import './menu.scss';
 
 const variants = {
@@ -14,13 +22,13 @@ const variants = {
 
 export const Navigation = () => (
     <motion.ul variants={variants}>
-        <MenuItem i={0} Title={'Home'} Links={'hero'} />
-        <MenuItem i={1} Title={'Skills'} Links={'skills'} />
-        <MenuItem i={2} Title={'Projects'} Links={'projects'} />
-        <MenuItem i={3} Title={'Services'} Links={'services'} />
-        <MenuItem i={4} Title={'Timeline'} Links={'timeline'} />
-        <MenuItem i={5} Title={'Testimonial'} Links={'testimonial'} />
-        <MenuItem i={6} Title={'Contact Me'} Links={'contact'} />
+        <MenuItem i={0} Title={'Home'} Links={'hero'} icon={<FaHome />} />
+        <MenuItem i={1} Title={'Skills'} Links={'skills'} icon={<GiSkills />}/>
+        <MenuItem i={2} Title={'Projects'} Links={'projects'} icon={<GoProjectSymlink />}/>
+        <MenuItem i={3} Title={'Services'} Links={'services'} icon={<GrServices />}/>
+        <MenuItem i={4} Title={'Timeline'} Links={'timeline'} icon={<MdViewTimeline />}/>
+        <MenuItem i={5} Title={'Testimonial'} Links={'testimonial'} icon={<MdRateReview />}/>
+        <MenuItem i={6} Title={'Contact Me'} Links={'contact'} icon={<RiContactsFill />}/>
     </motion.ul>
 );
 
